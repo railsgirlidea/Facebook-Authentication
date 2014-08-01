dbdbb25d6e74557de89e7422ad4358aab1b22bf7
Kalpana <kalpana.inkar@pragtech.co.in>
2014-08-01 18:10:27 +0530

    My Base code for successful login with facebook credential using Devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facebooktestapp::Application.config.secret_token = '18bbc9e91c6fd95770b0726878cc063a17a77da6d8d1004c2875cef5ac7bf5ba4fcd5a006e24887c170021a65f96fb18314b12aa99f7f78767829a9e575757a8'
